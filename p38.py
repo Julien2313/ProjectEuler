391bf6cf3f33ea01faed13322ae33a20a2023a1c
Julien2313 <julien@lorang.net>
2017-12-28 16:32:12 +0100

    Create p38.py

---
+++ p38.py
@@ -0,0 +1,7 @@
+def p38():
+  num = ""
+  x = 0
+  while len(num) <= 1000000:
+    num += str(x)
+    x += 1
+  print int(num[1]) * int(num[10]) * int(num[100]) * int(num[1000]) * int(num[10000]) * int(num[100000]) * int(num[1000000])
