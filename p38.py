61241b6d6105767543d49b865b33fd353ed0e46a
Julien2313 <julien@lorang.net>
2018-01-05 10:17:52 +0100

    Update p38.py

--- p38.py
+++ p38.py
@@ -1,7 +1,28 @@
 def p38():
-  num = ""
-  x = 0
-  while len(num) <= 1000000:
-    num += str(x)
-    x += 1
-  print int(num[1]) * int(num[10]) * int(num[100]) * int(num[1000]) * int(num[10000]) * int(num[100000]) * int(num[1000000])
+  maxi = 0
+  #max 9999 because 1xxxx will always be smaller
+  for num in xrange(0, 9999):
+    if str(num).count('0') > 0:
+      continue
+    strNum = ''
+    for n in xrange(1, 9):
+      strNum += str(num * n)
+      if len(strNum) < 9:
+        continue
+      if len(strNum) > 9:
+        break
+      if strNum.count('0') > 0:
+        break
+      
+          
+      occur     = Counter(strNum)
+      maxOccur  = max(occur.iterkeys(), key=(lambda key: occur[key]))
+      if occur[str(maxOccur)] > 1:
+        break
+      if n > 1:
+        if maxi < strNum:
+          maxi = strNum
+        break
+  print maxi
+
+p38()
