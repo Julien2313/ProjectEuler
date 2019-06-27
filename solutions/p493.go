30b52ac908a2431972065b4865d33bdc4fa39652
julien2313 <julien@lorang.net>
2019-06-27 15:45:32 +0200

    working without opti

--- solutions/p493.go
+++ solutions/p493.go
@@ -4,90 +4,45 @@ import (
 	"fmt"
 )
 
-const NBRBALLS = 2
-const NBRPICKS = 2
-const NBRBALLSPERCOLOR = 1
+const NBRCOLORS = 3
+const NBRPICKS = 4
+const NBRBALLSPERCOLOR = 2
 
-func P493() uint64 {
-
-	// 70 balles
-	// 7 couleurs
-	// 10 balles de chaque coulleur
-
-	// 20 tirages sans remise
-	/*
-		1er, on tir forcément 1 nouvelle couleur
-			[10, 10, 10, 10, 10, 10, 9]
-		2eme, on a 2 possibilités, soit on tir la même couleur, soit on en tir une nouvelle
-			[10, 10, 10, 10, 10, 10, 8] (9/69)
-
-			[10, 10, 10, 10, 10, 9, 9] (60/69)
-		3eme tirage, 3 possibilités
-			[10, 10, 10, 10, 10, 10, 7] (8/68)
-
-			[10, 10, 10, 10, 10, 9, 8] (10/68)
-
-			[10, 10, 10, 10, 9, 9, 9] (50/68)
-		4eme tirage, 4 possibilités
-			[10, 10, 10, 10, 10, 10, 6] (/67)
-			[10, 10, 10, 10, 10, 9, 7] (/67)
-
-			[10, 10, 10, 10, 10, 8, 7] (/67)
-			[10, 10, 10, 10, 10, 8, 7] (/67)
-			[10, 10, 10, 8, 9, 9, 9] (/67)
-	*/
+func P493() float64 {
 	balls := []int{}
-	for i := 0; i < NBRBALLS; i++ {
+	for i := 0; i < NBRCOLORS; i++ {
 		balls = append(balls, NBRBALLSPERCOLOR)
 	}
-	fmt.Println(RecurP493(0, 0, balls))
-	fmt.Println()
-	return 0
+	nbrColors, nbrCases := RecurP493(1, 0, balls)
+	fmt.Println(nbrColors, nbrCases)
+	return float64(nbrColors) / float64(nbrCases)
 }
 
-// nbr couleur/nbrlLancés
+// nbrCouleurs/nbrLancés
 func RecurP493(coef int, nbrPicks int, balls []int) (int, int) {
 	nbrColor := 0
 	if nbrPicks == NBRPICKS {
-		fmt.Println(balls)
-		// fmt.Println(balls)
-		for color := 0; color < NBRBALLS; color++ {
+		for color := 0; color < NBRCOLORS; color++ {
 			if balls[color] != NBRBALLSPERCOLOR {
 				nbrColor++
 			}
 		}
-		fmt.Println(nbrColor * coef)
-		return nbrColor * coef, coef
+		fmt.Println(balls)
+		return nbrColor, 1
 	}
 	nbrPicks++
-	futurCoef := 1
-	for color := 0; color < NBRBALLS; color++ {
+	totCoef := 0
+	for color := 0; color < NBRCOLORS; color++ {
 		if balls[color] == 0 {
 			continue
 		}
-		// if color+1 == NBRBALLS {
-		// 	balls[color]--
-
-		// 	newNbrColor := RecurP493(futurCoef, nbrPicks, balls)
-		// 	// nbrColor = (nbrColor*float64(coef) + newNbrColor*float64(newCoef)) / float64(newCoef+coef)
-		// 	nbrColor += newNbrColor
-
-		// 	balls[color]++
-		// } else {
-		// if balls[color+1] == balls[color] {
-		// 	futurCoef++
-		// } else {
 		balls[color]--
-
-		newNbrColor, newCoef := RecurP493(futurCoef, nbrPicks, balls)
-		// nbrColor = (nbrColor*float64(coef) + newNbrColor*float64(newCoef)) / float64(newCoef+coef)
-		coef += newCoef
-		nbrColor += newNbrColor
-
+		newNbrColor, newCoef := RecurP493(1, nbrPicks, balls)
 		balls[color]++
-		// futurCoef = 1.0
-		// }
-		// }
+
+		totCoef += newCoef * balls[color]
+		nbrColor += newNbrColor * balls[color]
 	}
+	coef *= totCoef
 	return nbrColor, coef
 }
